2abf449679a941fe6c39320b53e19ddb9f2fd987
Andrii Panasiuk <andriipanasiuk@gmail.com>
2015-10-28 00:48:33 +0200

    Add module with UI to play with bot

---
+++ MallorcaUI/src/mallorcatour/frames/ShowingSpectrumListener.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mallorcatour.frames;
+
+import mallorcatour.core.game.interfaces.ISpectrumListener;
+import mallorcatour.core.spectrum.Spectrum;
+
+/**
+ *
+ * @author Andrew
+ */
+public class ShowingSpectrumListener implements ISpectrumListener {
+
+    private boolean show = true;
+
+    public void setShow(boolean show) {
+        this.show = show;
+    }
+
+    public void onSpectrumChanged(Spectrum spectrum, String log) {
+        if (show) {
+            new SpectrumFrame(spectrum, log).setVisible(true);
+        }
+    }
+}
